a4e062be448afe1ed9abfd6ef5899739960a84cc
Tommy Brunn <tommy.brunn@blocket.se>
2014-08-13 13:40:20 +0200

    Add command-line interface.

--- main/main.go
+++ main/main.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/codegangsta/cli"
 	"github.com/nevon/gogel"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
 func main() {
-	doc := "<html><body><h1>Hello friends</h1><div id='myDiv'><p>This is a paragraph</p><p>Followed by another paragraph.</p></div></body></html>"
+	app := cli.NewApp()
+	app.Name = "gogel"
+	app.Usage = "Parse an HTML document"
+	app.Version = "0.0.1"
+	app.EnableBashCompletion = true
 
-	tree := gogel.Parse(doc)
-	Walk(tree[0], 0)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "html",
+			Usage: "path to HTML file to parse",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		html, err := ioutil.ReadFile(c.String("html"))
+
+		if err == nil {
+			tree := gogel.Parse(string(html))
+			Walk(tree[0], 0)
+		}
+	}
+
+	app.Run(os.Args)
 }
 
 func Walk(n gogel.Node, level int) {
